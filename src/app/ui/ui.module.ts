918c15a712038daf0a88be1a275a699ab15d42ef
christophe <christophegueroult@gmail.com>
2021-04-13 11:46:09 +0200

    ui end

--- src/app/ui/ui.module.ts
+++ src/app/ui/ui.module.ts
@@ -1,12 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { IconsModule } from '../icons/icons.module';
 import { UiComponent } from './components/ui/ui.component';
 import { Ui2Component } from './components/ui2/ui2.component';
 
 @NgModule({
   declarations: [UiComponent, Ui2Component],
-  imports: [CommonModule, IconsModule],
+  imports: [CommonModule],
   exports: [UiComponent, Ui2Component],
 })
 export class UiModule {}
